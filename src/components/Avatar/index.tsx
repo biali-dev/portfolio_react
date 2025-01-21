3e8f05438ff5960e44e48d2c9b2a7fb6d63de3e9
biali <ricardobiali@yahoo.com.br>
2025-01-21 20:22:15 -0300

    atualzacoes

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/biali-dev.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import Titulo from '../../components/Titulo'
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/biali-dev.png" />
-    <Titulo fontSize={20}>Ricardo Biali</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Ricardo Biali</Titulo>
+      <Paragrafo tipo="secundaria" fontSize={16}>
+        biali-dev
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Frontend
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
